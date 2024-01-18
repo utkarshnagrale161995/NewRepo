88ce189ad2b4f04f8bc6d0e10d4cd61481fdb86c
Utkarsh Manikrao Nagrale <utkarsh.nagrale@gmail.com>
2024-01-18 22:57:04 +0530

    commit files

---
+++ DevOpsUdemy/DevOpsUdemy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevopsUdemy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
