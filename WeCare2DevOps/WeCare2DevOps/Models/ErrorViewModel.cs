fbbd685b517146388669bd1c0e4cd072d36a5468
Utkarsh Manikrao Nagrale <utkarsh.nagrale@gmail.com>
2024-01-18 22:43:33 +0530

    change title on home page

---
+++ WeCare2DevOps/WeCare2DevOps/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WeCare2DevOps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
